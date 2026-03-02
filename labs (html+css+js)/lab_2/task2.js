5cbadbaa7e12996716d9ec9a1751718b4d9c15ec
smilezevil <demyanchuknastya2018@gmail.com>
2026-03-02 21:46:20 +0200

    add task2 for lab_2

---
+++ labs (html+css+js)/lab_2/task2.js
@@ -0,0 +1,15 @@
+function IsRange(el, num1, num2) {
+    return el >= num1 && el <= num2;
+}
+
+console.log(IsRange(5, 1, 10));  // true
+console.log(IsRange(15, 1, 10)); // false
+
+let isActive = true;
+let isActive2 = false;
+
+isActive = !isActive;
+isActive2 = !isActive2;
+
+console.log(`isActive: ${isActive}`);   // false
+console.log(`isActive2: ${isActive2}`); // true
