a674989d8e6d17614ab5ceb56e659fd74750be1e
smilezevil <demyanchuknastya2018@gmail.com>
2026-03-02 21:46:29 +0200

    add task3 for lab_2

---
+++ labs (html+css+js)/lab_2/task3.js
@@ -0,0 +1,60 @@
+function transferGradeToText(grade) {
+    if (grade >= 90 && grade <= 100) {
+        return "Відмінно";
+    }
+    else if (grade >= 75) {
+        return "Добре";
+    }
+    else if (grade >= 60) {
+        return "Задовільно";
+    }
+    else if (grade <= 59) {
+        return "Незадовільно";
+    }
+    else {
+        return "Невірна оцінка";
+    }
+}
+
+console.log(transferGradeToText(85),"\n");
+
+
+function transferGradeToTextShort(grade) {
+    return grade >= 90 ? "Відмінно" :
+            grade >= 75 ? "Добре" :
+                    grade >= 60 ? "Задовільно" :
+                            grade<=59 ?"Незадовільно":"Невірна оцінка";
+}
+
+console.log(transferGradeToTextShort(100),"\n");
+
+
+function transferSeasonToText(month) {
+    if (month>=1 && month<=2 || month==12) {
+        return "Зима";
+    }
+    else if (month>=3 && month<=5) {
+        return "Весна";
+    }
+    else if (month>=6 && month<=8) {
+        return "Літо";
+    }
+    else if (month>=9 && month<=11) {
+        return "Осінь";
+    }
+    else {
+        return "Невірний місяць";
+    }
+}
+
+console.log(transferSeasonToText(3),"\n");
+
+
+function transferSeasonToTextShort(month) {
+    return month>=1 && month<=2 || month==12 ? "Зима" :
+            month>=3 && month<=5 ? "Весна" :
+                    month>=6 && month<=8 ? "Літо" :
+                            month>=9 && month<=11 ? "Осінь" : "Невірний місяць";
+}
+
+console.log(transferSeasonToTextShort(6),"\n");
