bf568fd57be1280c16acacca674dcc4b88acf2e6
smilezevil <demyanchuknastya2018@gmail.com>
2026-03-11 15:51:05 +0200

    add task_3 for lab_3

---
+++ labs (html+css+js)/lab_3/task_3/task3.js
@@ -0,0 +1,42 @@
+let month = 10;
+
+switch (month) {
+    case 1:
+        console.log("Завдання 3: January");
+        break;
+    case 2:
+        console.log("Завдання 3: February");
+        break;
+    case 3:
+        console.log("Завдання 3: March");
+        break;
+    case 4:
+        console.log("Завдання 3: April");
+        break;
+    case 5:
+        console.log("Завдання 3: May");
+        break;
+    case 6:
+        console.log("Завдання 3: June");
+        break;
+    case 7:
+        console.log("Завдання 3: July");
+        break;
+    case 8:
+        console.log("Завдання 3: August");
+        break;
+    case 9:
+        console.log("Завдання 3: September");
+        break;
+    case 10:
+        console.log("Завдання 3: October");
+        break;
+    case 11:
+        console.log("Завдання 3: November");
+        break;
+    case 12:
+        console.log("Завдання 3: December");
+        break;
+    default:
+        console.log("Завдання 3: Invalid month");
+}
